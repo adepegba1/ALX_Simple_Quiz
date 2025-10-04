2049d299ddd375563458a42fbdcdebf0353ce582
adepegba1 <adepegbadavid@rocketmail.com>
2025-10-04 10:52:12 +0100

    1. Implement a Simple Interactive Quiz

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer() {
+  const feedback = document.getElementById("feedback");
+  const correctAnswer = "4";
+  const answer = document.querySelector(
+    "input[type='radio'][name='quiz']:checked"
+  );
+  const useranswer = answer.value;
+  if (useranswer == correctAnswer) {
+    feedback.textContent = "Correct! Well done.";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }
+}
+
+const submitButton = document.getElementById("submit-answer");
+submitButton.addEventListener("click", checkAnswer);
